52902b30dcff5783c45740b394b6a3a02a3eaa8f
maximiliann97 <maximilian@live.se>
2022-11-20 00:22:14 +0100

    Update VicsekModelFunctions.py

--- Homework2/VicsekModelFunctions.py
+++ Homework2/VicsekModelFunctions.py
@@ -4,8 +4,28 @@ import numpy as np
 
 def generate_particles(N, L):
     particles = np.random.randint(0, L, [N, 2])
-    for index, particle in enumerate(particles):
+    for _, particle in enumerate(particles):
         delta_rx = np.random.rand()
         delta_ry = np.random.rand()
-        particle[index]
+        particle[0] = particles[0] + delta_rx
+        particle[1] = particles[1] + delta_rx
     return particles
+
+
+def PBC(particles, L):
+    for _, particle in enumerate(particles):
+        if particle[0] < -L/2:
+            particle[0] = particle[0] + L
+        elif particle[0] > L/2:
+            particle[0] = particle[0] - L
+
+        if particle[1] < -L/2:
+            particle[1] = particle[1] + L
+        elif particle[1] > L/2:
+            particle[1] = particle[1] - L
+
+    return particles
+
+
+def determine_particles(particles, L):
+    
