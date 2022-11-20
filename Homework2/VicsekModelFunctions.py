293f9c2c95d95e6f8de1b68e03734633716027e5
maximiliann97 <maximilian@live.se>
2022-11-20 01:33:01 +0100

    Update VicsekModelFunctions.py

--- Homework2/VicsekModelFunctions.py
+++ Homework2/VicsekModelFunctions.py
@@ -1,17 +1,6 @@
 import numpy as np
 
 
-
-def generate_particles(N, L):
-    particles = np.random.randint(0, L, [N, 2])
-    for _, particle in enumerate(particles):
-        delta_rx = np.random.rand()
-        delta_ry = np.random.rand()
-        particle[0] = particle[0] + delta_rx
-        particle[1] = particle[1] + delta_rx
-    return particles
-
-
 def PBC(particles, L):
     for _, particle in enumerate(particles):
         if particle[0] < -L/2:
@@ -25,6 +14,12 @@ def PBC(particles, L):
             particle[1] = particle[1] - L
 
     return particles
+
+
+def generate_particles_positions(N, L):
+    coordinates = np.random.rand(N, 2) * 2 * L - L
+    theta = np.random.rand(N)*2*np.pi
+    return [coordinates, theta]
 
 
 def particles_in_radius(particles, L, r):
@@ -45,10 +40,10 @@ def get_velocity(theta, v=1):
     velocity = np.zeros([len(theta), 2])
     velocity[:, 0] = v*np.cos(theta)
     velocity[:, 1] = v*np.sin(theta)
-
     return velocity
 
 
-
-def get_global_alignment(velocity):
-    
+def get_global_alignment(velocity, v=1):
+    sum = np.sum(velocity, axis=0)/v
+    coeff = 1/len(velocity) * np.abs(sum)
+    return coeff
