3ae1d2c9dc9ed99c0f5123813660698c00a2dfa6
maximiliann97 <maximilian@live.se>
2022-11-28 00:12:18 +0100

    averaging

--- Homework3/average_recovered.py
+++ Homework3/average_recovered.py
@@ -1,4 +1,26 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
+# Initialize
+alpha_unicode = "\u03B1"
+gamma_unicode = "\u03B3"
+beta_unicode = "\u03B2"
+mu_unicode = "\u03BC"
+Beta = np.linspace(0.1, 0.9, 9)
+Gamma = [0.01, 0.02]
 
 # Load
+gamma_1 = np.load('R_inf_gamma=0.01_1.npy') + np.load('R_inf_gamma=0.01_2.npy') + np.load('R_inf_gamma=0.01_3.npy') + \
+          np.load('R_inf_gamma=0.01_4.npy') + np.load('R_inf_gamma=0.01_5.npy')
+
+gamma_2 = np.load('R_inf_gamma=0.02_1.npy') + np.load('R_inf_gamma=0.02_2.npy') + np.load('R_inf_gamma=0.02_1.npy') +\
+          np.load('R_inf_gamma=0.02_4.npy') + np.load('R_inf_gamma=0.02_5.npy')
+average_1 = gamma_1/5
+average_2 = gamma_2/5
+
+plt.plot(Beta, average_1, "o", color="blue", markersize=4)
+plt.plot(Beta, average_2, "o", color="green", markersize=4)
+plt.legend([f"{gamma_unicode}={Gamma[0]}", f"{gamma_unicode}={Gamma[1]}"])
+plt.xlabel(f'{beta_unicode}')
+plt.title(f"Final number of recovered agents as a function of the infection rate {beta_unicode} averaged over 5 iterations")
+plt.show()
