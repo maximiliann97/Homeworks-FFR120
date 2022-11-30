2b28fd792151f4f5aaca184903169c1a7d04922d
maximiliann97 <maximilian@live.se>
2022-11-30 22:32:00 +0100

    Update YearsInPrison.py

--- Homework4/YearsInPrison.py
+++ Homework4/YearsInPrison.py
@@ -4,16 +4,16 @@ import numpy as np
 def years(n, m, N, R, S, P):
     T = 0
     years_in_prison = np.zeros([N, 2])
-    player_A = [0]*n + [1]*(N-n)
-    player_B = [0]*m + [1]*(N-m)
+    player_A = [0]*n + [1]*(N-m)
+    player_B = [0]*m + [1]*(N-n)
 
     for i in range(N):
         if player_A[i] == 0 and player_B[i] == 0:
             continue
         elif player_A[i] == 0 and player_B[i] == 1:
-            player_A = [0]*i + [1]*(N-i)
+            player_A = np.where(player_A == 0, player_A, 1)
         elif player_A[i] == 1 and player_B[i] == 0:
-            player_B = [0]*i + [1]*(N-i)
+            player_B = np.where(player_B == 0, player_B, 1)
         else:
             continue
 
@@ -32,6 +32,7 @@ def years(n, m, N, R, S, P):
             years_in_prison[i, 1] = years_in_prison[i, 1] + P
     return years_in_prison
 
+
 m = 6
-n = 3
-print(years(m,m,1,2,3,4))
+n = 5
+print(years(n, m, 10, 2, 3, 4))
