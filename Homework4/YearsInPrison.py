086fdef16d0816f26df84f36eff9901c7ddec3c7
maximiliann97 <maximilian@live.se>
2022-12-01 00:15:49 +0100

    Update YearsInPrison.py

--- Homework4/YearsInPrison.py
+++ Homework4/YearsInPrison.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def years(n, m, N, R, S, P):
     T = 0
-    years_in_prison = np.zeros([N, 2])
-    player_A = [0]*n + [1]*(N-m)
-    player_B = [0]*m + [1]*(N-n)
+    years_in_prison = [0, 0]
+    player_A = [0]*n + [1]*(N-n)
+    player_B = [0]*m + [1]*(N-m)
 
     for i in range(N):
         if player_A[i] == 0 and player_B[i] == 0:
@@ -19,20 +19,24 @@ def years(n, m, N, R, S, P):
 
     for i in range(N):
         if player_A[i] == 0 and player_B[i] == 0:
-            years_in_prison[i, 0] = years_in_prison[i, 0] + R
-            years_in_prison[i, 1] = years_in_prison[i, 1] + R
+            years_in_prison[0] = years_in_prison[0] + R
+            years_in_prison[1] = years_in_prison[1] + R
         if player_A[i] == 0 and player_B[i] == 1:
-            years_in_prison[i, 0] = years_in_prison[i, 0] + S
-            years_in_prison[i, 1] = years_in_prison[i, 1] + T
+            years_in_prison[0] = years_in_prison[0] + S
+            years_in_prison[1] = years_in_prison[1] + T
         if player_A[i] == 1 and player_B[i] == 0:
-            years_in_prison[i, 0] = years_in_prison[i, 0] + T
-            years_in_prison[i, 1] = years_in_prison[i, 1] + S
+            years_in_prison[0] = years_in_prison[0] + T
+            years_in_prison[1] = years_in_prison[1] + S
         if player_A[i] == 1 and player_B[i] == 1:
-            years_in_prison[i, 0] = years_in_prison[i, 0] + P
-            years_in_prison[i, 1] = years_in_prison[i, 1] + P
+            years_in_prison[0] = years_in_prison[0] + P
+            years_in_prison[1] = years_in_prison[1] + P
     return years_in_prison
 
 
-m = 6
+m = 3
 n = 5
-print(years(n, m, 10, 2, 3, 4))
+N = 10
+R = 0.5
+S = 1.5
+P = 1
+print(years(n, m, N, R, S, P))
